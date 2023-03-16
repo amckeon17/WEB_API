b6d39052e3bd62446662dc3abaf2b91a1579e4da
amckeon17 <amckeon17@hotmail.com>
2023-03-16 10:43:51 -0700

    new update

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var startBtn = document.getElementById("start");
 var startPage = document.getElementById("startpage");
+var questionContainer = document.getElementById("questions");
 
-
+var questions = [
+    {
+        questionText: "what is 2+2?", 
+        questionAns: ["1", "2", "3", "4"],
+        correctAns: "4"
+    }
+]
+var questNum = 0;
 var secondsLeft = 60;
 console.log("connected")
 function setTime() {
@@ -27,11 +35,25 @@ function sendMessage() {
 
 }
 
-function hideElement() {
+function startQuiz() {
     startPage.classList.add("hide")
     setTime();
-
-
+    showQuestion();
 }
-startBtn.addEventListener("click", hideElement);
+startBtn.addEventListener("click", startQuiz);
+
+function showQuestion() {
+    var questEl = document.createElement("p");
+    questEl.textContent = questions[questNum].questionText;
+    questionContainer.appendChild(questEl);
+    for(var i = 0; i<questions[questNum].questionAns.length; i++) {
+        var ansEl = document.createElement("button");
+        ansEl.textContent = questions[questNum].questionAns[i];
+        questionContainer.appendChild(ansEl);
+        ansEl.addEventListener("click", function(event){
+            console.log(event.target.textContent)
+        })
+    }
+}
+
 
